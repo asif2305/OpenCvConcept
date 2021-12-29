41f9f721ef43509f4519c99a95791143d6011849
Asif Ahmed <48513167+asif2305@users.noreply.github.com>
2021-12-29 21:41:08 +0100

    creating hand tracking module

---
+++ Image Processing/Application/MyHandGaming.py
@@ -0,0 +1,26 @@
+import cv2
+import mediapipe as mp
+import time
+import HandTrackingModule as htm
+
+
+# frame rate
+p_time = 0
+c_time = 0
+cap = cv2.VideoCapture(0)
+detector = htm.handDetector()
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+        print(lmList[4])
+
+    c_time = time.time()
+    fps = 1 / (c_time - p_time)
+    p_time = c_time
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
